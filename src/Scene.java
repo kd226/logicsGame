1c574d61a32356bf2b95aa8f20e63260d7700779
Konrad Dobros <kd226622@gmail.com>
2016-05-20 17:34:20 +0200

    Lost message fix

--- src/Scene.java
+++ src/Scene.java
@@ -8,25 +8,25 @@ import java.util.Random;
 import jline.ConsoleReader;
 
 public class Scene {
-	
+
 	private QuestionBar qb;
 	private Person suspects[];
 	private String chat[];
 	private int chatLength;
 	private int State; // 0 - game rollin, 1 - won, 2 - lost, 3 - escape pressed
 	private int questionsAsked;
-	
-	private String progressBar(Duration time){ 
+
+	private String progressBar(Duration time){
 		// Print progress bar to console, with color modifications
 		String ret = "Q: "+ questionsAsked + " Time: ";
 		if (time.toMinutes()<2) ret +="\033[31m"; // Print time in red if less than 2 minutes
-		ret += String.format("%02d",time.toMinutes()) + ":" 
+		ret += String.format("%02d",time.toMinutes()) + ":"
 		+ String.format("%02d",(time.getSeconds() - time.toMinutes()*60)) + " \033[39m[";
-		
+
 		if (time.toMinutes()<2) ret +="\033[31m"; // Print bar in red if less than 2 minutes
 		else if (time.toMinutes()<5) ret +="\033[33m"; // Print in orange if less than 5
 		else ret +="\033[32m"; // Print in green otherwise
-		
+
 		for (int i = 0; i<60; i++){
 			if (i<time.getSeconds()/60.0*4) ret += "=";
 			else ret += " ";
@@ -37,13 +37,13 @@ public class Scene {
 		}
 		return ret + "\n";
 	}
-	
+
 	public void render() throws IOException{
 		// Game initialization
 		Duration counter = Duration.ofMinutes(15); // Game time count down
 		Duration deltaTime = Duration.ZERO; // Time since last tick
 		Instant tick = Instant.now(); // Tick instant from clock
-	    System.out.print("\033[2J\033[?25l"); // Clear the screen and hide the cursor 
+	    System.out.print("\033[2J\033[?25l"); // Clear the screen and hide the cursor
 		ConsoleReader reader = new ConsoleReader();
 		reader.getTerminal().disableEcho();
 	    // Scene initialization
@@ -57,7 +57,7 @@ public class Scene {
 	    // Initialize suspects
 	    suspects = new Person[5];
 	    Random generator = new Random();
-	    int killer = generator.nextInt(suspects.length); // Random killer 
+	    int killer = generator.nextInt(suspects.length); // Random killer
 	    int liar = generator.nextInt(suspects.length); // Random liar
 	    for (int i = 0; i<suspects.length; i++){
 	    	int guiltTable[] = new int [3];
@@ -69,7 +69,7 @@ public class Scene {
 	    	suspects[i].killer = false;
 	    	suspects[i].lying = false;
 	    }
-	    suspects[liar].lying = true;	
+	    suspects[liar].lying = true;
 	    suspects[killer].killer = true;
 	    suspects[killer].motive = 1;
 	    suspects[killer].weapon = 1;
@@ -79,20 +79,20 @@ public class Scene {
 	    suspects[2].name = "\033[36;1mRoman\033[39;22m";
 	    suspects[3].name = "\033[32;1mJerzy\033[39;22m";
 	    suspects[4].name = "\033[31;1mTadeusz\033[39;22m";
-	    		
+
 	    qb = new QuestionBar(suspects);
 		while(State == 0 && !counter.isZero()){
 			// Render the game screen
 		    System.out.print("\033[H"
-		    		+ ""); // Position cursor at 1,1	
+		    		+ ""); // Position cursor at 1,1
 			System.out.print(progressBar(counter));
 			// Print chat starting from oldest messages
 			for (int i = (chatLength+1)%chat.length; i != chatLength;
-					i = (i+1)%chat.length){ 
+					i = (i+1)%chat.length){
 				System.out.println(chat[i]);
 			}
 			System.out.println(chat[chatLength]);
-			
+
 			System.out.print(qb.render());
 		    System.out.flush();
 		    // Read input
@@ -121,7 +121,7 @@ public class Scene {
 						if (newChat != ""){
 							chatLength = (chatLength + 1)%chat.length;
 							chat[chatLength] = newChat;
-							if (newChat.equals("Well played!")) State = 1; 
+							if (newChat.equals("Well played!")) State = 1;
 							else if (newChat.equals("You lost!")) State = 2;
 							else {
 								questionsAsked++;
@@ -132,7 +132,7 @@ public class Scene {
 				}
 				System.out.print("\033[2J");
 			}
-		
+
 			// Time actualization
 			deltaTime = Duration.between(tick, Instant.now());
 			tick = Instant.now();
@@ -141,9 +141,9 @@ public class Scene {
 		// Game ended
 		System.out.print("\n\n" + chat[chatLength] + "\n\n");
 		if (State == 1){ // Win message
-			System.out.print("You asked " + questionsAsked + " questions in " 
+			System.out.print("You asked " + questionsAsked + " questions in "
 		+ Logic.valued + "-valued logic." + "\n");
-			System.out.print("Figuring out the truth took you " 
+			System.out.print("Figuring out the truth took you "
 		+ (Duration.ofMinutes(15).minus(counter).toMinutes()) + " minutes and "
 		+ (Duration.ofMinutes(15).minus(counter).getSeconds()-Duration.ofMinutes(15).minus(counter).toMinutes()*60)
 		+ " seconds.\n");
@@ -152,16 +152,16 @@ public class Scene {
 		if (State == 2){
 			System.out.println("The real killer was... ");
 			for (int i = 0; i< suspects.length; i++){
-				if (suspects[i].killer) System.out.println("\t" + suspects[i].name);			
+				if (suspects[i].killer) System.out.println("\t" + suspects[i].name);
 			}
 			System.out.println("And the liar was...");
-			for (int i = 0; i< 3; i++){
-				if (suspects[i].lying) System.out.println("\t" + suspects[i].name);			
+			for (int i = 0; i< suspects.length; i++){
+				if (suspects[i].lying) System.out.println("\t" + suspects[i].name);
 			}
 
 		}
-		
+
 		System.out.print("\033[0m\033[?25h");
 		reader.getTerminal().enableEcho();
-	}	
+	}
 }
